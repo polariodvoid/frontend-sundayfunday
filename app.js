8ded7d93fa06f12e3d30683c63d6c23a08252286
Abe Fi <a06afilaka@gmail.com>
2022-08-01 22:56:17 -0400

    The menu on index is less glaring and i attempted to add, the menu bar

--- app.js
+++ app.js
@@ -1,38 +1,3 @@
-//const email = document.getElementById('email')
-//const password = document.getElementById('password')
-//const form = document.getElementById('form')
-//const errorElement = document.getElementById('loginForm')
-
-//form.addEventListener('submit', (e) => {
-//audio for seltzer
-//let beat = new Audio('../images/pour-beer-into-glass-2-31585.mp3');
-//document.getElementById("myBtn").addEventListener("click", function() {
-    //const newLocal = beat.play();
-  //});
-
-
-//let messages = []
-  //if (email.value === '' || email.value == null) {
-    //messages.push('Email is required')
-  //}
-
-  //if (password.value.length <= 6) {
-    //messages.push('Password must be longer than 6 characters')
-  //}
-
-  //if (password.value.length >= 20) {
-    //messages.push('Password must be less than 20 characters')
-  //}
-
-  //if (password.value === 'password') {
-    //messages.push('Password cannot be password')
-  //}
-
- // if (messages.length > 0) {
-   // e.preventDefault()
-    //errorElement.innerText = messages.join(', ')
-  //}
-//})
 
 
 function validate() {
@@ -78,9 +43,9 @@ function validate() {
     } else {
       length.style.color = "green";
     }
-  }
+}
   
-  function confirmPassword() {
+function confirmPassword() {
     let password1 = document.querySelector("#pass1");
     let password2 = document.querySelector("#pass2");
   
